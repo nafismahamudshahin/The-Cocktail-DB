a939a3d813918b645578037014d695301b090bc4
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-08-10 22:12:46 +0600

    display all drinks by search

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loadData = (search)=>{
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`)
     .then(res=>res.json())
     .then(data=>{
-        console.log(data.drinks);
+        displayData(data.drinks);
     })
 }
 
@@ -13,4 +13,29 @@ btn.addEventListener('click',(e)=>{
     // clear search box:
     document.getElementById('search_box').value="";
 })
+loadData('a');
 
+const displayData = (drinks)=>{
+    console.log(drinks[0]);
+    const drinksContainer = document.getElementById('drinks_container');
+    drinks.forEach(drink=>{
+        const div = document.createElement('div');
+        div.classList.add('col')
+        div.innerHTML=`
+            <div class="card h-100">
+                <img src="${drink.strDrinkThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${drink.strGlass}</h5>
+                    <h6 class="card-title">Category: ${drink.strCategory}</h6>
+
+                    <p class="card-text">${drink.strInstructions.slice(0,60)}...</p>
+                </div>
+                    <div class="card-footer cart_bottom">
+                        <a href="#" class="btn btn-primary">Add to Cart</a>
+                        <a href="#" class="btn btn-success">Details</a>
+                    </div>
+            </div>
+        `;
+        drinksContainer.appendChild(div)
+    })
+}
